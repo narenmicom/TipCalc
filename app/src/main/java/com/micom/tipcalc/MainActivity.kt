8c5bb2ee10b481336b106e1b7e7ca88784dd82aa
Narayan Kumar <narenmicom@gmail.com>
2022-02-20 22:38:36 +0530

    Fully Developed app

--- app/src/main/java/com/micom/tipcalc/MainActivity.kt
+++ app/src/main/java/com/micom/tipcalc/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.micom.tipcalc
 
+import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 private const val TAG = "MainActivity"
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvtipbar: TextView
     private lateinit var tvtipans: TextView
     private lateinit var tvtotalans: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +33,13 @@ class MainActivity : AppCompatActivity() {
 
         tipseekbar.progress = INITIAL_TIP_PERCENT
         tvtipbar.text = "$INITIAL_TIP_PERCENT%"
+        tipdescription(INITIAL_TIP_PERCENT)
         tipseekbar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
 //                Log.i(TAG, "onProgressChanged $p1")
                 tvtipbar.text = "$p1%"
                 computeTipAndTotal()
+                tipdescription(p1)
 
             }
 
@@ -57,6 +61,7 @@ class MainActivity : AppCompatActivity() {
             override fun afterTextChanged(p0: Editable?) {
 //                Log.i(TAG, "afterTextChanges $p0")
                 computeTipAndTotal()
+
             }
 
         }
@@ -66,6 +71,23 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+
+    private fun tipdescription(tip: Int) {
+        val tipdesc = when(tip){
+            in 0..9 -> "Poor"
+            in 10..14 -> "Acceptable"
+            in 15..19 -> "Good"
+            in 20..24 -> "Excellent"
+            else -> "Amazing"
+        }
+        tvtipdesc.text = tipdesc
+        val color = ArgbEvaluator().evaluate(
+            tip.toFloat() / tipseekbar.max,
+            ContextCompat.getColor(this,R.color.worst_tip),
+            ContextCompat.getColor(this,R.color.best_tip)
+        ) as Int
+        tvtipdesc.setTextColor(color)
     }
 
     private fun computeTipAndTotal() {
